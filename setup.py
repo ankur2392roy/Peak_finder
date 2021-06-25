d1b5e90049e670ea7ad8d9ca2edc33c9c2209403
Ishan Mishra <im356@cornell.edu>
2021-06-25 14:04:35 -0400

    added functionality to extract required packages from requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
 
+def get_requires():
+    reqs = []
+    for line in open('requirements.txt', 'r').readlines():
+        reqs.append(line)
+    return reqs
+
 setup(
      name='peak_finder',
      version = '0.1',
-     packages=find_packages()
+     packages=find_packages(),
+     install_requires=get_requires()
 )
